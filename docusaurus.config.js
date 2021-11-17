46936fd2fddaae6c6d337eee9d3ce6a6ea834cf4
isaackwok <kch062522@gmail.com>
2021-11-17 22:43:11 +0800

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,112 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Webcipe',
+  tagline: '你的 Web 開發食譜',
+  url: 'https://webcipe.dev',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'isaackwok', // Usually your GitHub org/user name.
+  projectName: 'isaackwok.github.io', // Usually your repo name.
+  trailingSlash: false,
+  deploymentBranch: 'main',
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/isaackwok/isaackwok.github.io',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/isaackwok/isaackwok.github.io',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Webcipe | 你的 Web 開發食譜',
+        logo: {
+          alt: 'Webcipe',
+          src: 'img/favicon.ico',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'right',
+            label: 'Notes',
+          },
+          { to: '/blog', label: 'Blog', position: 'right' },
+          {
+            href: 'https://github.com/isaackwok',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Notes',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Facebook',
+                href: 'https://www.facebook.com/isaac.0625/',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Webcipe by Isaac Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
